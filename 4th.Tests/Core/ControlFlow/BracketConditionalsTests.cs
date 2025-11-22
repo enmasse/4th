6e14fef3ffd22225c63c3792dd847f50546b7c61
CI User <ci@example.com>
2025-11-22 22:45:31 +0100

    Add BracketConditionalsTests for [IF] [ELSE] [THEN] and interpret-time IF behavior

---
+++ 4th.Tests/Core/ControlFlow/BracketConditionalsTests.cs
@@ -0,0 +1,55 @@
+using System.Threading.Tasks;
+using Xunit;
+using Forth.Core.Interpreter;
+
+namespace Forth.Tests.Core.ControlFlow
+{
+    public class BracketConditionalsTests
+    {
+        [Fact]
+        public async Task BracketIF_True_ExecutesFirstPart()
+        {
+            var forth = new ForthInterpreter();
+            Assert.True(await forth.EvalAsync("1 [IF] 2 [ELSE] 3 [THEN]"));
+            Assert.Single(forth.Stack);
+            Assert.Equal(2L, (long)forth.Stack[0]);
+        }
+
+        [Fact]
+        public async Task BracketIF_False_ExecutesElsePart()
+        {
+            var forth = new ForthInterpreter();
+            Assert.True(await forth.EvalAsync("0 [IF] 2 [ELSE] 5 [THEN]"));
+            Assert.Single(forth.Stack);
+            Assert.Equal(5L, (long)forth.Stack[0]);
+        }
+
+        [Fact]
+        public async Task BracketIF_Nested_SkipsCorrectly()
+        {
+            var forth = new ForthInterpreter();
+            // Outer true, inner false chooses inner ELSE
+            Assert.True(await forth.EvalAsync("1 [IF] 0 [IF] 10 [ELSE] 20 [THEN] [ELSE] 30 [THEN]"));
+            Assert.Single(forth.Stack);
+            Assert.Equal(20L, (long)forth.Stack[0]);
+        }
+
+        [Fact]
+        public async Task InterpretIF_True_RunsThenPart()
+        {
+            var forth = new ForthInterpreter();
+            Assert.True(await forth.EvalAsync("1 IF 11 ELSE 22 THEN"));
+            Assert.Single(forth.Stack);
+            Assert.Equal(11L, (long)forth.Stack[0]);
+        }
+
+        [Fact]
+        public async Task InterpretIF_False_RunsElsePart()
+        {
+            var forth = new ForthInterpreter();
+            Assert.True(await forth.EvalAsync("0 IF 11 ELSE 22 THEN"));
+            Assert.Single(forth.Stack);
+            Assert.Equal(22L, (long)forth.Stack[0]);
+        }
+    }
+}
